aeb3d90f26489705f2041cf3c806b5fe3256860d
Luay <luayghazwan@yahoo.com>
2018-01-09 11:48:39 -0800

    Startup file configured with Routing template and mvc middleware

--- DontWineAboutIt/DontWineAboutIt/Startup.cs
+++ DontWineAboutIt/DontWineAboutIt/Startup.cs
@@ -15,6 +15,8 @@ namespace DontWineAboutIt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add MVC Middleware
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +26,17 @@ namespace DontWineAboutIt
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Default routing template
+            app.UseMvc(route =>
+            {
+                route.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}/");
+            });
+
+            // Required to use static files such as CSS and CSV
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
