757dcdbe7a05a6949289dc7d1fea9b407198700d
Luay <luayghazwan@yahoo.com>
2018-01-09 11:43:16 -0800

    Basic scaffolding finished

---
+++ DontWineAboutIt/DontWineAboutIt/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DontWineAboutIt.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return RedirectToAction("Results");
+        }
+
+        [HttpGet]
+        public IActionResult Results()
+        {
+            return View();
+        }
+    }
+}
---
+++ DontWineAboutIt/DontWineAboutIt/Models/Wine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DontWineAboutIt.Models
+{
+    public class Wine
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public string Designation { get; set; }
+        public string Points { get; set; }
+        public int Price { get; set; }
+        public string Province { get; set; }
+        public string Region1 { get; set; }
+        public string Region2 { get; set; }
+        public string Variety { get; set; }
+        public string Winery { get; set; }
+    }
+}
