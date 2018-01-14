5e33955065b4851db3ec98fbd8a2c10420858bda
Luay <luayghazwan@yahoo.com>
2018-01-13 22:15:11 -0800

    Removed unnecessary libraries

--- DontWineAboutIt/XUnitTestDontWineAboutIt/UnitTest1.cs
+++ DontWineAboutIt/XUnitTestDontWineAboutIt/UnitTest1.cs
@@ -3,13 +3,6 @@ using Xunit;
 using DontWineAboutIt.Controllers;
 using DontWineAboutIt.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using DontWineAboutIt.Controllers;
-using Xunit;
 
 namespace XUnitTestDontWineAboutIt
 {
