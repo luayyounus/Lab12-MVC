89b4691881d58e19295cac7ff696c2ae60dc6095
Luay <luayghazwan@yahoo.com>
2018-01-13 21:23:10 -0800

    Test implemented

--- DontWineAboutIt/XUnitTestDontWineAboutIt/UnitTest1.cs
+++ DontWineAboutIt/XUnitTestDontWineAboutIt/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using DontWineAboutIt.Controllers;
 
 namespace XUnitTestDontWineAboutIt
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void GetMainIndexPage()
         {
+            // Arrange
+            var homeController = new HomeController();
 
+            // Act
+            var result = homeController.Index();
+            string viewName = result.ToString();
+
+            // Assert
+            Assert.True(!string.IsNullOrEmpty(viewName));
         }
     }
 }
